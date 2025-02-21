158a246af926c9f1b6d5725f155d8791a9d72036
filiperak <filip.erak@googlemail.com>
2025-02-21 14:04:18 +0100

    fix: update CORS configuration to allow requests from any origin

--- index.js
+++ index.js
@@ -23,8 +23,9 @@ const expressServer = app.listen(PORT,() => {
 
 const io = new Server(expressServer,{
     cors:{
-        origin: process.env.NODE_ENV === "production"?
-        false : ["http://localhost:5500","http://127.0.0.1:5500"]
+        // origin: process.env.NODE_ENV === "production"?
+        // false : ["http://localhost:5500","http://127.0.0.1:5500"]
+        origin:["*"]
     }
 })
 
