59a7b0e97b0800e23c1bcde4176374a312eb8fec
filiperak <filip.erak@googlemail.com>
2025-02-21 14:42:08 +0100

    .

--- index.js
+++ index.js
@@ -21,13 +21,14 @@ const expressServer = app.listen(PORT,() => {
     console.log(`App is running on ${PORT}`);  
 })
 
-const io = new Server(expressServer,{
-    cors:{
-        // origin: process.env.NODE_ENV === "production"?
-        // false : ["http://localhost:5500","http://127.0.0.1:5500"]
-        origin:["*"]
-    }
-})
+// const io = new Server(expressServer,{
+//     cors:{
+//         // origin: process.env.NODE_ENV === "production"?
+//         // false : ["http://localhost:5500","http://127.0.0.1:5500"]
+//         origin:["*"]
+//     }
+// })
+const io = new Server(expressServer)
 
 //init socket logic
 handleSocket(io)
